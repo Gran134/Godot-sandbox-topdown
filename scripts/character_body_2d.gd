60e6c57f31dcc61c04b3d94de7ef4629e2d23919
Gran235 <sindresbrekke@gmail.com>
2025-11-08 15:06:44 +0100

    made zooming to the camera

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -5,7 +5,8 @@ var last_direction := Vector2(1,0)
 
 func _physics_process(_delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_upp", "move_down")
-	velocity = direction * max_speed
+	
+	velocity = direction * max_speed / $Camera2D.zoom * 3
 	move_and_slide()
 	
 	if direction.length() > 0:
