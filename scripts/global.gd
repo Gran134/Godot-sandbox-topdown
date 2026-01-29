af37032ba981e198484ba8a7fba235df8c3411cd
Gran235 <sindresbrekke@gmail.com>
2026-01-29 09:46:10 +0100

    settings

---
+++ scripts/global.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var where_am_i: String
+
+func _ready() -> void:
+	where_am_i = "res://scenes/main_menu.tscn"
--- scripts/settings.gd
+++ scripts/settings.gd
@@ -1,6 +1,11 @@
 extends Control
 
-var where_am_i = "res://scenes/mane_menu.tscn"
 
 func back() -> void:
-	get_tree().change_scene_to_file(where_am_i)
+	get_tree().change_scene_to_file(Global.where_am_i)
+	
+func _process(_delta: float) -> void:
+	if get_tree().current_scene.name == "res://scenes/main_menu.tscn":
+		Global.where_am_i = "res://scenes/main_menu.tscn"
+	elif get_tree().current_scene.name == "res://scenes/background.tscn":
+		Global.where_am_i = "res://scenes/background.tscn"
